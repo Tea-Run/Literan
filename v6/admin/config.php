25771967fb770db1c8650550501ee06839fc160c
Tea-Run <22030482@itcelaya.edu.mx>
2026-03-28 00:44:29 -0600

    Eliminé carpeta

--- v6/admin/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-SESSION_START();
-
-define ('DBDRIVER', 'pgsql');
-define ('DBHOST', 'postgres');
-define ('DBNAME', 'database');
-define ('DBUSER', 'user');
-define ('DBPASSWORD', 'password');
-define ('DBPORT', '5432');
--- v6/js/alerts.js
+++
@@ -1,6 +0,0 @@
-setTimeout(() => {
-    const alert = document.querySelector('.border-l-4');
-    if(alert){
-        alert.remove();
-    }
-}, 4000);
